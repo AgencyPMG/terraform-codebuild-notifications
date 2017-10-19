bb73d9ed0caaa355f3ef6f3f16cba5810accf77b
Christopher Davis <cdavis9999@gmail.com>
2017-10-19 09:13:43 -0500

    Add a Topic Policy That Allows CloudWatch Events

--- main.tf
+++ main.tf
@@ -2,6 +2,24 @@ resource "aws_sns_topic" "builds" {
     name = "${var.topic_name}"
 }
 
+data "aws_iam_policy_document" "builds" {
+    statement {
+        sid = "TrustCloudWatchEvents"
+        effect = "Allow"
+        resources = ["${aws_sns_topic.builds.arn}"]
+        actions = ["sns:Publish"]
+        principals {
+            type = "Service"
+            identifiers = ["events.amazonaws.com"]
+        }
+    }
+}
+
+resource "aws_sns_topic_policy" "builds_events" {
+    arn = "${aws_sns_topic.builds.arn}"
+    policy = "${data.aws_iam_policy_document.builds.json}"
+}
+
 resource "aws_cloudwatch_event_rule" "builds" {
     name = "codebuild-to-${var.topic_name}"
     event_pattern = <<PATTERN
